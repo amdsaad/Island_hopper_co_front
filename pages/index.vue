f832f0413b87250d92f42421f80f2d5858dfcd53
amdsaad <saadamd@gmail.com>
2023-04-12 00:27:12 +0200

    handling error

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container mx-auto text-lg py-8 px-4">
-    <h1 class="title text-center font-bold text-2xl">Welcome to the Default Page</h1>
+    <h1 class="title text-center font-bold text-2xl">
+      Welcome to the Default Page
+    </h1>
     <div
       v-for="article in articles"
       :key="article.id"
       class="my-4 w-full max-w-[800px] p-4 mx-auto"
     >
-      <h2 class="font-bold text-xl ">{{ article.attributes.title }}</h2>
+      <h2 class="font-bold text-xl">{{ article.attributes.title }}</h2>
       <p class="my-4">{{ article.attributes.description }}</p>
       <p class="text-xl leading-10 my-4">{{ article.attributes.body }}</p>
     </div>
@@ -16,7 +18,12 @@
 <script setup>
 import { allArticlesQuery } from "~/graphql/queries";
 
-const { data } = await useAsyncQuery(allArticlesQuery);
+const { data, error } = await useAsyncQuery(allArticlesQuery);
+
+if (error) {
+  console.log(error);
+  // pls handle error here
+}
 const articles = data.value.articles.data;
 </script>
 

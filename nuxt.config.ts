ae4ce5858bd493484410d7d6f954f011fca25c9d
amdsaad <saadamd@gmail.com>
2023-04-12 00:26:00 +0200

    adding tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/apollo", "@nuxtjs/eslint-module"],
+  modules: ["@nuxtjs/apollo", "@nuxtjs/eslint-module", "@nuxtjs/tailwindcss"],
   apollo: {
     autoImports: true,
     clients: {
@@ -9,4 +9,5 @@ export default defineNuxtConfig({
       },
     },
   },
+  tailwindcss: {},
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <h1 class="title">Welcome to the Default Page</h1>
-    <div v-for="article in articles" v-bind:key="article.id">
-      <h2>{{ article }}</h2>
+  <div class="container mx-auto text-lg py-8 px-4">
+    <h1 class="title text-center font-bold text-2xl">Welcome to the Default Page</h1>
+    <div
+      v-for="article in articles"
+      :key="article.id"
+      class="my-4 w-full max-w-[800px] p-4 mx-auto"
+    >
+      <h2 class="font-bold text-xl ">{{ article.attributes.title }}</h2>
+      <p class="my-4">{{ article.attributes.description }}</p>
+      <p class="text-xl leading-10 my-4">{{ article.attributes.body }}</p>
     </div>
   </div>
 </template>
